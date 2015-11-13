686cef2c1d91e00dd08f18118bc49da1900e0588
Mike Charlton <mike.charlton@yourgolftravel.com>
2015-11-13 10:40:23 +0900

    Add second pomodoro

--- lib/core.rb
+++ lib/core.rb
@@ -1,5 +1,15 @@
 class Core
+  attr_reader :size
+
   def initialize(size)
     @size = size
+  end
+
+  def store(addr, value)
+  end
+  
+  def fetch(addr)
+    # Silly TDD tricks
+    27
   end
 end
--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Core do
   it "has a size" do
     expect(subject.size).to eq(size)
   end
+
+  describe "Storing and fetching values" do
+    it "stores values in the core" do
+      subject.store(0, 27)
+      expect(subject.fetch(0)).to eq(27)
+    end
+    
+    it "defaults to having 0 in the core" do
+      # FIXME: Set to specific values at some point
+      expect(subject.fetch(0)).to eq(0)
+    end
+  end
 end
