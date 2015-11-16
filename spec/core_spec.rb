38dd426a1e1df365af69bfeca165906b66a62da5
Mike Charlton <mike.charlton@yourgolftravel.com>
2015-11-16 09:54:50 +0900

    Add code for session 3

--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -16,10 +16,26 @@ RSpec.describe Core do
       subject.store(0, 27)
       expect(subject.fetch(0)).to eq(27)
     end
+
+    it "should do nothing when storing to an address of size or bigger" do
+      subject.store(size, 27)
+      expect(subject.fetch(size)).to be_nil
+    end
+
+    it "should do nothing when storing to negative addresses" do
+      subject.store(-1, 27)
+      expect(subject.fetch(-1)).to be_nil
+    end
     
     it "defaults to having 0 in the core" do
       # FIXME: Set to specific values at some point
       expect(subject.fetch(0)).to eq(0)
+    end
+
+    # We're already testing size.  I could delete this one,
+    # but why not have an extra one for size + 1
+    it "returns nil for addresses bigger than size" do
+      expect(subject.fetch(size + 1)).to be_nil
     end
   end
 end
