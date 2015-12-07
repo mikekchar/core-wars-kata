122efba66d5b106ed7c32b5544f14ec3aafd872c
Mike Charlton <mike.charlton@yourgolftravel.com>
2015-12-07 18:07:01 +0900

    Add 20th session
    Sketching out the DAT instruction

--- lib/instruction.rb
+++ lib/instruction.rb
@@ -1,9 +1,41 @@
+class Operand
+  attr_reader :mode, :number
+
+  def initialize(mode, number)
+    @mode = mode
+    @number = number
+  end
+
+  def eql?(other)
+    other.mode == @mode && other.number == @number
+  end
+end
+
 class Instruction
   attr_reader :opcode, :modifier, :a, :b
+
   def initialize(opcode, modifier, a, b)
     @opcode = opcode
     @modifier = modifier
     @a = a
     @b = b
+  end
+
+  def eql?(other)
+    other.opcode == @opcode && other.modifier == @modifier &&
+      other.a == @a && other.b == @b
+  end
+end
+
+class Dat < Instruction
+  def initialize(a, b)
+    @opcode = "DAT"
+    @modifier = "F"
+    @a = a
+    @b = b
+  end
+
+  def Dat.match?(instruction)
+    instruction.opcode == "DAT"
   end
 end
