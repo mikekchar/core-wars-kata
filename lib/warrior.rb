b4e0644808693f5b8d8d83bbf6606de7f7583b76
Mike Charlton <mike.charlton@yourgolftravel.com>
2016-01-06 11:05:57 +0900

    Add session 38
    Add tasks

--- lib/warrior.rb
+++ lib/warrior.rb
@@ -1,18 +1,18 @@
+require_relative "./task_queue"
+
 class Warrior
   def initialize(mars, address)
     @mars = mars
     @address = address
-    # The pc should really be on the task, but
-    # we haven't got that far, so I'll put it here for now.
-    @pc = address
+    @tasks = TaskQueue.new(mars)
+    @tasks.new_task(address)
   end
 
   def step
-    # This shouldn't be here either.
-    @pc = @mars.address(@pc + 1)
+    @tasks.step()
   end
 
   def to_s
-    "PC:#{@pc}"
+    @tasks.status.join("\n")
   end
 end
