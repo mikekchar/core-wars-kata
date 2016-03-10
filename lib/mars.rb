3baf833d8bebc8f05fb309bda72b833c78ee75bd
Mike Charlton <mike.charlton@yourgolftravel.com>
2016-03-10 15:45:12 +0900

    Add tests for Mars

--- lib/mars.rb
+++ lib/mars.rb
@@ -6,10 +6,10 @@ require_relative "./log"
 class Mars
   attr_reader :warriors, :log
 
-  def initialize(core, logWriter)
+  def initialize(core)
     @core = core
     @warriors = []
-    @log = Log.new(logWriter)
+    @log = Log.new()
   end
 
   def address(addr)
@@ -26,21 +26,21 @@ class Mars
 
   def addWarrior(addr)
     warrior = Warrior.new(@warriors.length, @core, addr)
+    @log.add(Event.new("Warrior", @warriors.length, "added"))
     @warriors << warrior
   end
 
   def removeDeadWarriors
     @warriors.each.with_index do |warrior, i|
       if warrior.killed?
-        @warriors.delete_at(i)
         @log.add(Event.new("Warrior", i, "killed"))
+        @warriors.delete_at(i)
       end
     end
   end
 
   def step(addr)
     if !addr.nil?
-      @log.add(Event.new("Warrior", @warriors.length, "added"))
       addWarrior(addr)
     end
     @warriors.each { |warrior| warrior.step() }
